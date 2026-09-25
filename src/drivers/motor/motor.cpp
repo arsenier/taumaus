f3d6c8bf2aa227856141bc7722c3e4eb79607b13
Arsenii <yarmolinskiyam@gmail.com>
2026-09-25 19:22:52 +0300

    feat: basic motor driver

--- src/drivers/motor/motor.cpp
+++ src/drivers/motor/motor.cpp
@@ -4,16 +4,19 @@
 
 namespace drivers::motor
 {
-    void init()
+    void MotorDriver::init()
     {
-        pinMode(9, OUTPUT);
-        pinMode(10, OUTPUT);
+        pinMode(dir_pin, OUTPUT);
+        pinMode(pwm_pin, OUTPUT);
     }
 
-    void set_voltage(float voltage)
+    void MotorDriver::set_voltage(float u)
     {
-        analogWrite(9, voltage);
-        analogWrite(10, voltage);
+        digitalWrite(dir_pin, u > 0);
+
+        int pwm = u / 9.0 * 255;
+
+        analogWrite(pwm_pin, pwm);
     }
 
 }; // namespace drivers::motor
